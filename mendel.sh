74e9945f6b85105651aaea01053e273eec215935
Glenn Hickey <glenn.hickey@gmail.com>
2023-10-17 12:02:25 -0400

    fix

--- mendel.sh
+++ mendel.sh
@@ -20,16 +20,16 @@ bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" -o mendel_tmp_${NAME
 tabix -fp vcf mendel_tmp_${NAME}
 
 # run rtg
-rtg mendelian -i mendel_tmp_${NAME} -t ${SDF} --pedigree ped_tmp_${NAME}.ped > ${VCF::-6}.mendel
+rtg mendelian -i mendel_tmp_${NAME} -t ${SDF} --pedigree ped_tmp_${NAME}.ped > ${VCF::-7}.mendel
 
 rm -f mendel_tmp_${NAME} mendel_tmp_${NAME}.tbi
 
-# make a phony heterozygous
-bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\""  | sed -e 's#0/0#0/1#g' -e 's#1/1#0/1#g' -e 's#2/2#0/2#g' | bgzip --threads 2 > mendel_het_tmp_${NAME}
+# make a phony heterozygous (what about 0/0?)
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\""  | sed  -e 's#1/1#0/1#g' -e 's#2/2#0/2#g' | bgzip --threads 2 > mendel_het_tmp_${NAME}
 tabix -fp vcf mendel_het_tmp_${NAME}
 
 # run rtg
-rtg mendelian -i mendel_het_tmp_${NAME} -t ${SDF} --pedigree ped_tmp_${NAME}.ped > ${VCF::-6}.mendel.het
+rtg mendelian -i mendel_het_tmp_${NAME} -t ${SDF} --pedigree ped_tmp_${NAME}.ped > ${VCF::-7}.mendel.het
 
 rm -f mendel_het_tmp_${NAME} mendel_het_tmp_${NAME}.tbi
 
