83ad72436a428fa8a1d3a7f3425a6fa737b68adc
Glenn Hickey <glenn.hickey@gmail.com>
2023-10-17 11:37:34 -0400

    update

--- vcfstats.sh
+++ vcfstats.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 VCF=$1
+MIN_QUAL=$2
 
 # ts/tv
-bcftools stats ${VCF} | grep "^TSTV" | head -1 | awk '{print "TSTV\t" $5}'
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" | bcftools stats | grep "^TSTV" | head -1 | awk '{print "TSTV\t" $5}'
 
 # counts
-bcftools stats ${VCF} | grep "^SN" | grep "number of records" | awk '{print "Total\t" $6}'
-bcftools stats ${VCF} | grep "^SN" | grep "number of SNPs" | awk '{print "SNPs\t" $6}'
-bcftools stats ${VCF} | grep "^SN" | grep "number of indels" | awk '{print "Indels\t" $6}'
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" | bcftools stats | grep "^SN" | grep "number of records" | awk '{print "Total\t" $6}'
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" | bcftools stats | grep "^SN" | grep "number of SNPs" | awk '{print "SNPs\t" $6}'
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" | bcftools stats | grep "^SN" | grep "number of indels" | awk '{print "Indels\t" $6}'
 
 
