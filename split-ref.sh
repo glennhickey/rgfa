c7d4c0265d833f9cf3b199c6fbf3a6caf1ade053
Glenn Hickey <glenn.hickey@gmail.com>
2023-10-24 12:58:49 -0400

    update

--- split-ref.sh
+++ split-ref.sh
@@ -3,12 +3,12 @@
 VCF=$1
 PREFIX=$2
 
-OUT_NAME=${VCF::-6}.onref.vcf.gz
+OUT_NAME=${VCF::-7}.onref.vcf.gz
 bcftools view ${VCF} -h | bgzip --threads 2 > $OUT_NAME
 bcftools view ${VCF} -H | grep ^${PREFIX} | bgzip --threads 2 >> $OUT_NAME
 tabix -fp vcf ${OUT_NAME}
 
-OUT_NAME=${VCF::-6}.offref.vcf.gz
+OUT_NAME=${VCF::-7}.offref.vcf.gz
 bcftools view ${VCF} -h | bgzip --threads 2 > $OUT_NAME
 bcftools view ${VCF} -H | grep -v ^${PREFIX} | bgzip --threads 2 >> $OUT_NAME
 tabix -fp vcf ${OUT_NAME}
