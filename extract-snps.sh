0739964bb3ea4c3e181ec714e26455533bad8ba6
Glenn Hickey <glenn.hickey@gmail.com>
2023-10-24 20:26:59 -0400

    fix

--- extract-snps.sh
+++ extract-snps.sh
@@ -6,9 +6,3 @@ SNP_NAME=${VCF::-7}.snp-only.vcf.gz
 
 strip-nested.py $VCF 1 | bcftools +fill-tags | bcftools view -i "STRLEN(REF) < 2 && STRLEN(ALT) < 2 && AC > 1" | bcftools view -i 'FILTER="." || FILTER="PASS"' | bgzip > ${SNP_NAME}
 tabix -fp vcf ${SNP_NAME}
-
-
-
-
-
-														       }
