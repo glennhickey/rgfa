bc9b474ae22258ad436a733f7062a235949f4fbb
Glenn Hickey <glenn.hickey@gmail.com>
2023-10-24 20:30:31 -0400

    fix

--- extract-snps.sh
+++ extract-snps.sh
@@ -4,5 +4,10 @@ VCF=$1
 
 SNP_NAME=${VCF::-7}.snp-only.vcf.gz
 
-strip-nested.py $VCF 1 | bcftools +fill-tags | bcftools view -i "STRLEN(REF) < 2 && STRLEN(ALT) < 2 && AC > 1" | bcftools view -i 'FILTER="." || FILTER="PASS"' | bgzip > ${SNP_NAME}
+# need temp file to fix the header
+strip-nested.py $VCF 1 | bgzip > ${SNP_NAME}.tmp.gz
+tabix -fp vcf ${SNP_NAME}.tmp.gz
+bcftools +fill-tags ${SNP_NAME}.tmp.gz | bcftools view -i "STRLEN(REF) < 2 && STRLEN(ALT) < 2 && AC > 1" | bcftools view -i 'FILTER="." || FILTER="PASS"' | bgzip > ${SNP_NAME}
 tabix -fp vcf ${SNP_NAME}
+rm -f ${SNP_NAME}.tmp.gz ${SNP_NAME}.tmp.gz.tbi
+
