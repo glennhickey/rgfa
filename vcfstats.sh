a134da304e215a44f8412b334493923f58cdfd05
Glenn Hickey <glenn.hickey@gmail.com>
2023-11-03 10:57:22 -0400

    faster

--- vcfstats.sh
+++ vcfstats.sh
@@ -3,10 +3,10 @@
 VCF=$1
 MIN_QUAL=$2
 
-OUT_NAME_1=${VCF::-7}.vcfstats.tmp.1
-OUT_NAME_2=${VCF::-7}.vcfstats.tmp.1
-OUT_NAME_3=${VCF::-7}.vcfstats.tmp.1
-OUT_NAME_4=${VCF::-7}.vcfstats.tmp.1
+OUT_NAME_1=${VCF::-7}.vcfstats.${MIN_QUAL}.tmp.1
+OUT_NAME_2=${VCF::-7}.vcfstats.${MIN_QUAL}.tmp.1
+OUT_NAME_3=${VCF::-7}.vcfstats.${MIN_QUAL}.tmp.1
+OUT_NAME_4=${VCF::-7}.vcfstats.${MIN_QUAL}.tmp.1
 
 # ts/tv
 bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" | bcftools stats | grep "^TSTV" | head -1 | awk '{print "TSTV\t" $5}' > ${OUT_NAME_1} &
@@ -18,7 +18,7 @@ bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" | bcftools stats | g
 
 wait
 
-OUT_NAME=${VCF::-7}.stats.tsv
+OUT_NAME=${VCF::-7}.stats.${MIN_QUAL}.tsv
 cat ${OUT_NAME_1} ${OUT_NAME_2} ${OUT_NAME_3} ${OUT_NAME_4} > ${OUT_NAME}
 rm -f ${OUT_NAME_1} ${OUT_NAME_2} ${OUT_NAME_3} ${OUT_NAME_4}
 
