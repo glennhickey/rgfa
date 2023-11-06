b1a42447adae67dd1649b7d57820da9239a4b31c
Glenn Hickey <glenn.hickey@gmail.com>
2023-11-06 12:19:10 -0500

    update

--- mendel.sh
+++ mendel.sh
@@ -4,7 +4,7 @@ VCF=$1
 SDF=$2
 MIN_QUAL=$3
 
-NAME="$(basename ${VCF}).${MIN_QUAL}"
+NAME="$(basename ${VCF})"
 
 # make the pedigree
 printf "1 HG001 0 0 1 0\n
@@ -13,28 +13,28 @@ printf "1 HG001 0 0 1 0\n
 4 HG004 0 0 2 0\n
 5 HG005 HG006 HG007 1 0\n
 6 HG006 0 0 1 0\n
-7 HG007 0 0 2 0\n" > ped_tmp_${NAME}.ped
+7 HG007 0 0 2 0\n" > ped_tmp_${MIN_QUAL}_${NAME}.ped
 
 set -x
 
 # do some filtering
-bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" -o mendel_tmp_${NAME} -Oz
-tabix -fp vcf mendel_tmp_${NAME}
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\"" -o mendel_tmp_${MIN_QUAL}_${NAME} -Oz
+tabix -fp vcf mendel_tmp_${MIN_QUAL}_${NAME}
 
 # run rtg
-rtg mendelian -l -i mendel_tmp_${NAME} -t ${SDF} --pedigree ped_tmp_${NAME}.ped > ${VCF::-7}.${MIN_QUAL}.mendel
+rtg mendelian -l -i mendel_tmp_${MIN_QUAL}_${NAME} -t ${SDF} --pedigree ped_tmp_${MIN_QUAL}_${NAME}.ped > ${VCF::-7}.${MIN_QUAL}.mendel
 
-rm -f mendel_tmp_${NAME} mendel_tmp_${NAME}.tbi
+rm -f mendel_tmp_${MIN_QUAL}_${NAME} mendel_tmp_${MIN_QUAL}_${NAME}.tbi
 
 # make a phony heterozygous (what about 0/0?)
-bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\""  | sed  -e 's#1/1#0/1#g' -e 's#2/2#0/2#g' | bgzip --threads 2 > mendel_het_tmp_${NAME}
-tabix -fp vcf mendel_het_tmp_${NAME}
+bcftools view $VCF -i "QUAL>${MIN_QUAL} && FILTER=\"PASS\""  | sed  -e 's#1/1#0/1#g' -e 's#2/2#0/2#g' | bgzip --threads 2 > mendel_het_tmp_${MIN_QUAL}_${NAME}
+tabix -fp vcf mendel_het_tmp_${MIN_QUAL}_${NAME}
 
 # run rtg
-rtg mendelian -l -i mendel_het_tmp_${NAME} -t ${SDF} --pedigree ped_tmp_${NAME}.ped > ${VCF::-7}.${MIN_QUAL}.mendel.het
+rtg mendelian -l -i mendel_het_tmp_${MIN_QUAL}_${NAME} -t ${SDF} --pedigree ped_tmp_${MIN_QUAL}_${NAME}.ped > ${VCF::-7}.${MIN_QUAL}.mendel.het
 
-rm -f mendel_het_tmp_${NAME} mendel_het_tmp_${NAME}.tbi
+rm -f mendel_het_tmp_${MIN_QUAL}_${NAME} mendel_het_tmp_${MIN_QUAL}_${NAME}.tbi
 
-rm -f ped_tmp_${NAME}.ped
+rm -f ped_tmp_${MIN_QUAL}_${NAME}.ped
 
 
