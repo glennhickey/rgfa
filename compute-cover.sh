a7038fda0429a1fdebc9187d7c887a671a272c10
Glenn Hickey <glenn.hickey@gmail.com>
2024-04-22 16:30:32 -0400

    new script

---
+++ compute-cover.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ex
+
+# compute rgfa cover
+
+# input GFA.gz file
+INPUT_GFA=$1
+
+# output GBZ file (will include cover as ref path fragments)
+OUTPUT_GBZ=$2
+
+# min interval size
+MIN_INTERVAL=$3
+
+# ref prefix
+REF_PREFIX=$4
+
+MAX_THREADS=32
+
+VG_RGFA=~/dev/vg.rgfa/bin/vg
+# there is a bug (feature?) in master that randomly converts W-lines to P-lines that
+# seems to throw everything off.  So stick to a release for conversion
+VG=/private/groups/cgl/cactus/cactus-bin-v2.8.1/bin/vg
+
+# the above bug also prevents vg.rgfa from reading GFA, hence the extra convert step at beginning
+zcat ${INPUT_GFA} | ${VG} convert - | ${VG_RGFA} paths -x - -f ${MIN_INTERVAL} -Q ${REF_PREFIX} -t ${MAX_THREADS} | ${VG} convert -f - > ${OUTPUT_GBZ}.gfa 
+${VG} gbwt -G ${OUTPUT_GBZ}.gfa --gbz-format -g ${OUTPUT_GBZ}
+rm -f  ${OUTPUT_GBZ}.gfa
