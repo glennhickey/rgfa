bd6a6dbc3910660827b2ded8a3ba9e4788f2debf
Glenn Hickey <glenn.hickey@gmail.com>
2024-07-31 11:06:19 -0400

    update

--- parallel-deconstruct.sh
+++ parallel-deconstruct.sh
@@ -33,9 +33,10 @@
 
 L=1
 
-for arg ; do echo "${arg} ${arg::-3}.vcf.gz" ; done | parallel -j 32 "vg deconstruct {1}  -n -P GRCh38 | bgzip > {2} ; tabix -fp {2}"
-#note the sed bit applies only to v1.1 hprc graphs, which dont have reference haplotypes (something since changed in cactus)
-bcftools concat -a *.vcf.gz | sed -e 's/GRCh38/GRCh38#0/g' -e 's/CHM13/CHM13#0/g' | bgzip > out.L${L}.vcf.gz
-tabix -fp vcf out.L${L}.vcf.gz
+for arg ; do echo "${arg}" ; done | parallel -j 32 "vg deconstruct {} -n -P GRCh38 | bgzip > {.}.vcf.gz ; tabix -fp vcf {.}.vcf.gz"
 
+rm -f out.L${L}.vcf.gz
+#note the sed bit applies only to v1.1 hprc graphs, which dont have reference haplotypes (something since changed in cactus)
+bcftools concat -a *.vcf.gz | sed -e 's/GRCh38/GRCh38#0/g' -e 's/CHM13/CHM13#0/g' | bgzip > out.L${L}.vcf.gz;
+tabix -fp vcf out.L${L}.vcf.gz
 
