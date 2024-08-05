1cc0c1a52c13803bfa22a61d8e2963feab9b4b44
Glenn Hickey <glenn.hickey@gmail.com>
2024-08-05 12:15:11 -0400

    update

--- pangenie-all.sh
+++ pangenie-all.sh
@@ -16,12 +16,12 @@ sbatch --wait pangenie.sh ${PG_VCF} ${INPUT_FA} 7 &
 wait
 
 # resolve the genotypes
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG001_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG002_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG003_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG004_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG005_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG006_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG007_genotpying.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG001_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG002_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG003_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG004_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG005_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG006_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG007_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
 
 wait
