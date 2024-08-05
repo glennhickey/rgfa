c5c9ee0f01c37670072b9b5daf94f83ac1024aa9
Glenn Hickey <glenn.hickey@gmail.com>
2024-08-05 13:05:48 -0400

    update

--- pangenie-all.sh
+++ pangenie-all.sh
@@ -18,11 +18,11 @@ wait
 
 # resolve the genotypes
 sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG001_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG002_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG003_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG004_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG005_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG006_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
-sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG007_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG001_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG002_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG002_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG003_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG003_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG004_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG004_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG005_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG005_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG006_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG006_resolved.vcf.gz &
+sbatch --wait resolve-pangenie.sh ${INPUT_VCF} ${PG_VCF::-4}.pg.HG007_genotyping.vcf.gz ${PG_VCF::-4}.pg.HG007_resolved.vcf.gz &
 
 wait
